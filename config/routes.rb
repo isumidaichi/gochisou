e36d83e9fbe53cd1ba234c9dd50a9ef62f0a7ba5
daichi <anaminkumamoto@gmail.com>
2018-10-08 21:45:50 +0900

    add_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
+  get '/' => 'posts#index'
+
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
