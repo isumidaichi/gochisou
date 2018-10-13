371df44097a5ad287b1c01ea6f77801dadfa34e7
daichi <anaminkumamoto@gmail.com>
2018-10-13 17:15:50 +0900

    create_index_p

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,18 @@
 class PostsController < ApplicationController
+
   def index
   end
+
+  def show
+  end
+
+  def host
+  end
+
+  def input
+  end
+
+  def confirm
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/show'
 
   get '/' => 'posts#index'
+  get '/post/input' => 'posts#input'
+  get '/post/confirm/:id' => 'posts#confirm'
+  get '/post/:id' => 'posts#show'
+  get '/host/:id' => 'posts#host'
+
+  get '/user/:id' => 'users#show'
+  # get '/authenticate' => 'users#authenticate'
 
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
